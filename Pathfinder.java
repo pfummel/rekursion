c4fe8d0480f7a18de40972639bc565b2d6677909
pfummel <greenpfum-348@yahoo.de>
2016-01-31 21:41:04 +0100

    Pathfinder-Klasse + Test erstellt

---
+++ Pathfinder.java
@@ -0,0 +1,47 @@
+/**
+Klasse Pathfinder
+@author David Nancekievill MATRIKELNUMMER Gruppe 9c
+@author Markus Berning MATRIKELNUMMER Gruppe 9c
+*/
+public class Pathfinder implements Simple {
+
+    Pathfinder() {
+    }    
+    
+    private char[][] map;
+    
+    private int int xStart, yStart, xGoal, yGoal = 0;
+    
+    public String searchPath(char start, char goal) {
+        
+        /*
+        Ermittelt die Koordinaten der Anfangs- und Endpunkte
+        */
+        int i, j = 0;
+        
+        for ( ; i < map.length; i++) {
+            for ( ; j < map.length; j++) {
+                if (map[i][j] == start) {
+                    xStart = i;
+                    yStart = j;
+                }
+                if (map[i][j] == goal) {
+                    xGoal = i;
+                    yGoal = j;
+                }
+            }
+        }
+        
+        
+        
+        return;
+    }
+    
+    public void setMap(char[][] map) {
+        this.map = map;
+    }
+    
+    public char[][] getMap() {
+        return this.map;
+    }    
+}
---
+++ Test.java
@@ -0,0 +1,18 @@
+/**
+Klasse Test
+@author David Nancekievill MATRIKELNUMMER Gruppe 9c
+@author Markus Berning MATRIKELNUMMER Gruppe 9c
+*/
+public class Test {
+    public static void main(String[] args) {
+    
+    char[][] map = {{'#','#','#','#','#'},
+                    {'#','S','#',' ',' '},
+                    {'#',' ',' ',' ','#'},
+                    {'#',' ','#',' ','#'},
+                    {'#','#','#','X','#'}}
+    Pathfinder pf = new Pathfinder();
+    pf.setMap(map);
+    System.out.println(pf.searchPath('S','X'));
+    }
+}
